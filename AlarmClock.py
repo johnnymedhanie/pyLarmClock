24cfff517bfe9f9e4780583274c807513d77db13
Jonathan Medhanie <johnnymedhanie@users.noreply.github.com>
2017-05-16 21:41:57 -0700

    first commit
    no testing yet -- just barebones of code. so far can:
    
    open link from collections when triggered

---
+++ AlarmClock.py
@@ -0,0 +1,42 @@
+import os
+import time
+import webbrowser
+import sys
+import builtins
+import random
+
+if os.path.isfile("collection.txt") == False:
+    print
+    'FATAL: collections doesn\'t '
+    print
+    "Creating a file now.."
+    op = os.O_CREAT | os.O_WRONLY | os.O_RDONLY
+    create = os.open("collection.txt", op)
+    with os.fdopen(create, 'w') as newCollection:
+        newCollection.write("https://www.youtube.com/watch?v=GibiNy4d4gc")
+
+print
+"When do you want to start your alarm?"
+print
+"Input as 00:00"
+alarmTime = input("time?")
+
+with open("collection.txt") as collection:
+    videos = collection.readline()
+
+
+currentTime = time.strftime("%H:%M")
+
+if currentTime != alarmTime:
+    print('it is currently' + currentTime + 'and your alarm is set for' + alarmTime)
+    currentTime = time.strftime("%H:%M")
+    time.sleep(5)
+
+if currentTime == alarmTime:
+    print("Wake up")
+    linkvideo = random.choice(videos)
+    webbrowser.open(linkvideo)
+
+
+
+
