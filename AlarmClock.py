39f134a2e1ab78674f201fae86f3957fdf4c0599
johnnymedhanie <jonathanisahyuga@gmail.com>
2017-05-16 22:16:41 -0700

    fixed minor bugs
    used while instead of if. oops.

--- AlarmClock.py
+++ AlarmClock.py
@@ -10,15 +10,13 @@ if os.path.isfile("collection.txt") == False:
     'FATAL: collections doesn\'t '
     print
     "Creating a file now.."
-    op = os.O_CREAT | os.O_WRONLY | os.O_RDONLY
+    op = os.O_CREAT | os.O_WRONLY
     create = os.open("collection.txt", op)
     with os.fdopen(create, 'w') as newCollection:
         newCollection.write("https://www.youtube.com/watch?v=GibiNy4d4gc")
 
-print
-"When do you want to start your alarm?"
-print
-"Input as 00:00"
+print("When do you want to start your alarm?")
+print("Input as 00:00")
 alarmTime = input("time?")
 
 with open("collection.txt") as collection:
@@ -27,7 +25,7 @@ with open("collection.txt") as collection:
 
 currentTime = time.strftime("%H:%M")
 
-if currentTime != alarmTime:
+while currentTime != alarmTime:
     print('it is currently' + currentTime + 'and your alarm is set for' + alarmTime)
     currentTime = time.strftime("%H:%M")
     time.sleep(5)
